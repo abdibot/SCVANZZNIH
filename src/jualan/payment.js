ae53e0c8db3e59d938725ac3abf08621e1c187c7
abdibot <abdialamanah75@gmail.com>
2022-03-25 21:52:26 +0800

    SCVANZZNIH

---
+++ src/jualan/payment.js
@@ -0,0 +1,11 @@
+exports.payment = (prefix) => {
+return  `*- PAYMENT*
+
+• GOPAY : 62858-2462-9954
+• DANA : 62858-2462-9954
+• OVO : 62858-2462-9954
+• LINK AJA : 62858-2462-9954
+
+_Untuk pembayaran via qris silakan chat owner untuk meminta qris_
+_Jika sudah di transfer harap kirim bukti transfer ke owner bot !!_`
+}
---
+++ src/jualan/sewabot.js
@@ -0,0 +1,10 @@
+exports.sewabot = (prefix) => {
+return  `*- SEWABOT*
+
+• 1 MINGGU : 1.000-3.000
+• 1 BULAN : 5.000-7.000
+• PERMANEN : 10.000-12.000
+
+_Untuk pembayaran silakan ketik_ *${prefix}payment*
+_Untuk kalian yang minat silakan hubungin owner bot_`
+}
